657e7074cdc489d59b5aab6c2a45ba5a7b8eb7c3
mzaini30 <muhzaini30@gmail.com>
2020-10-12 23:37:42 +0800

    :rocket: Selesai
    Alhamdulillah

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,45 @@
-<script>
-	export let name;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+<div class="container">
+	<br>
+	<h5 class="text-center">Khatam</h5>
+	{#if data.length > 0}
+		<hr>
+		<p>Kamu sudah khatam sebanyak <strong>{data.length}</strong> kali</p>
+		<ol class="jarak-bawah">
+			{#each data as x}
+				<li><strong>{new Date(x.tanggal).getDate()} {namaBulan[new Date(x.tanggal).getMonth()]} {new Date(x.tanggal).getFullYear()}</strong> (<em>{moment(x.tanggal).fromNow()}</em>) <span class="hapus" on:click={hapus(x.id)}>[hapus]</span></li>
+			{/each}
+		</ol>
+	{/if}
+	<div class="melayang btn btn-info" on:click={tambah}>&plus;</div>
+</div>
+<style type="text/css">
+	.melayang {
+		position: fixed;
+		right: 20px;
+		bottom: 20px;
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	.hapus {
+		cursor: pointer;
 	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.jarak-bawah {
+		margin-bottom: 75px;
 	}
 </style>
+<script type="text/javascript">
+	import {onMount} from 'svelte'
+	let data = []
+	const namaBulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember']
+	const tampil = () => db.data.reverse().toArray(x => data = x)
+	const hapus = (n) => {
+		const tanya = confirm('Hapus kah?')
+		if (tanya) {
+			db.data.delete(n).then(() => tampil())
+		}
+	}
+	onMount(() => tampil())
+	const tambah = () => {
+		let kalender = new Date
+		let tanggal = `${kalender.getFullYear()}-${kalender.getMonth() + 1}-${kalender.getDate()} ${kalender.getHours()}:${kalender.getMinutes()}:${kalender.getSeconds()}`
+		db.data.add({tanggal: tanggal}).then(() => tampil())
+	}
+</script>
