ce0f0a652ed7409dde6d9b09c68fc31e39e49004
mzaini30 <muhzaini30@gmail.com>
2021-07-16 09:50:55 +0800

    upload

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,22 @@
 <script setup>
 	document.title = 'Khatam'
+	import tambah from '/src/icon/tambah.vue'
 </script>
 
 <template>
 	<div class="p-3">
 		<div class="message">
-			<div class="message-header list" v-for='x in Array(10)'>
+			<div class="message-header list has-background-success" v-for='x in Array(20)'>
 				2021-08-13 08:13:11
 				<div class="delete"></div>
 			</div>
 		</div>
-		<router-link to="/credit" class='has-text-centered is-block'>Credit</router-link>
+		<div class="icon">
+			<tambah></tambah>
+		</div>
+		<p class="has-text-centered mb-3">
+			<router-link to="/credit">Credit</router-link>
+		</p>
 	</div>
 </template>
 
@@ -19,5 +25,14 @@
 		border-radius: 0;
 		font-weight: normal;
 		border: 1px solid white;
+	}
+	.icon {
+		--posisi: 30px;
+		cursor: pointer;
+		position: fixed;
+		display: block;
+		right: var(--posisi);
+		bottom: var(--posisi);
+		color: #3e8ed0!important;
 	}
 </style>
